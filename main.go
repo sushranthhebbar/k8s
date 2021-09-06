70e80649f96600578c07435a180bb333feca77dd
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-09-06 21:38:28 +0530

    Fixed handler issue

--- main.go
+++ main.go
@@ -10,23 +10,25 @@ func main() {
     started := time.Now()
     fmt.Println("Starting hello-world server...")
     http.HandleFunc("/", helloServer)
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        panic(err)
-    }
 
     http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
         duration := time.Now().Sub(started)
+        fmt.Println("In.")
         if duration.Seconds() > 10 {
             w.WriteHeader(500)
             w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+            fmt.Println(">10")
         } else {
             w.WriteHeader(200)
             w.Write([]byte("ok"))
+            fmt.Println("<10")
         }
     })
-
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        panic(err)
+    }
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello world!")
+    fmt.Fprint(w, "Hello world")
 }
