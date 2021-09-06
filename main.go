6bc163ee8f83e9ad75fd22b21ab6f5cef1682f4d
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-09-06 20:15:01 +0530

    Added /healthz

--- main.go
+++ main.go
@@ -11,6 +11,18 @@ func main() {
     if err := http.ListenAndServe(":8080", nil); err != nil {
         panic(err)
     }
+
+    http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        duration := time.Now().Sub(started)
+        if duration.Seconds() > 10 {
+            w.WriteHeader(500)
+            w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+        } else {
+            w.WriteHeader(200)
+            w.Write([]byte("ok"))
+        }
+    })
+
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
